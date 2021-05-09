ab7fe00b0ace0551c472f9956b70c744243111a7
Daniel Sundermeier <daniel@hof-sundermeier.de>
2021-05-09 12:22:17 +0200

    Update index.blade.php

--- resources/views/bluehpatenschaft/index.blade.php
+++ resources/views/bluehpatenschaft/index.blade.php
@@ -6,7 +6,7 @@
     <div class="lg:flex mt-6 max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div class="lg:w-1/2">
             <p class="my-3">Insekten sind unverzichtbar für unser Ökosystem. Sie übernehmen nicht nur die wichtige Rolle der Bestäubung für viele Pflanzen und sorgen so dafür, dass wir als Menschen Ernten einfahren und uns davon ernähren können. Sie selbst sind ebenfalls Nahrungsmittelgrundlage für viele andere Tierarten, wie beispielsweise Vögel. Außerdem sorgen Insekten für eine verbesserte Bodenqualität und sind natürliche Schädlingsbekämpfer.</p>
-            <p class="my-3">Dass Bienen und andere Insekten heute aber immer weniger Lebensraum für sich finden, hat viele verschiedene Gründe. Unter anderem zählen die Intensivierung der Landwirtschaft, die Lichtverschmutzung in und um Siedlungen oder der Einsatz von Pflanzenschutzmitteln dazu.</p>
+            <p class="my-3">Bienen und andere Insekten finden aber immer weniger Lebensraum.</p>
             <p class="my-3">Höchste Zeit also, diesen kleinen Bewohnern der Erde ein neues Zuhause zu bieten! Wir würden gerne unseren Beitrag leisten und haben eine Ackerfläche zur Verfügung gestellt und zur Blühwiese gemacht.</p>
             <p class="my-3">Durch das Anlegen einer Blühwiese haben wir einen Ort geschaffen, der vom Menschen unberührt bleibt und an dem sich Insekten wohl fühlen und gerne leben. Damit können wir alle gemeinsam zum Artenschutz beitragen.</p>
             <p class="my-3">Mitmachen kann jeder, der sich für Insekten und Naturschutz engagieren möchte und sich an einer blühenden Wiese mit Gesumme erfreut.</p>
