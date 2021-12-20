df7cc1a999958cd28b8c78afa5ef1c5f2c9c29b2
Daniel Sundermeier <daniel@hof-sundermeier.de>
2021-12-20 02:33:27 +0100

    Update create.blade.php

--- resources/views/bluehpatenschaft/create.blade.php
+++ resources/views/bluehpatenschaft/create.blade.php
@@ -1,7 +1,7 @@
 <div class="py-20 bg-white" id="form">
 
     <div class="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 class="text-3xl sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl mb-6">Blühpate für Blühsaison 2020 werden</h2>
+        <h2 class="text-3xl sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl mb-6">Blühpate für Blühsaison 2022 werden</h2>
         <form action="{{ route('bluehpatenschaft.store') }}" method="POST">
             @csrf
 
